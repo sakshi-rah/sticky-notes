e6c03b318b0ca30be8a8d94cfa21032cf7f227ae
sakshi rahangdale <rahangdalesakshi2@gmail.com>
2022-12-10 18:57:40 +0530

    added pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css';
+import AddNote from './View/AddNote/AddNote';
+import Home from './View/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" elements={<Home/>}/>
+          <Route path="/addnote" elements={<AddNote/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/View/AddNote/AddNote.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./AddNote.css"
+
+function AddNote() {
+  return (
+    <div>AddNote</div>
+  )
+}
+
+export default AddNote
