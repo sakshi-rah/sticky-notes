c6170275dab25e940c277628aeccebfbfb6d6f77
sakshi rahangdale <rahangdalesakshi2@gmail.com>
2022-12-12 13:45:51 +0530

    final commit

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ### This is Sticky-Notes website its making by using of react js. its small  day to day life activities storage you can also use it .
 
-![Live Preview](./public/Screenshot/cartoon.png)
+![Live Preview](./public/Screenshot/sticky-notes.jpg)
 
-[See Live Demo]()
+[See Live Demo](https://store-notes-app.netlify.app)
--- src/View/Home/Home.js
+++ src/View/Home/Home.js
@@ -15,16 +15,16 @@ function Home() {
   ])
 
   // triggers initially
-  useEffect(()=>{
+  useEffect(() => {
     const notes = localStorage.getItem("notes")
-    if(notes){
+    if (notes) {
       setNotes(JSON.parse(notes))
     }
   }, [])
 
   // triggers when notes changes
-  useEffect(()=>{
-    if(notes.length > 1){
+  useEffect(() => {
+    if (notes.length > 1) {
       localStorage.setItem("notes", JSON.stringify(notes))
     }
   }, [notes])
@@ -32,7 +32,7 @@ function Home() {
   const [title, setTitle] = useState("")
   const [content, setContent] = useState("")
 
-  function addNote(){
+  function addNote() {
     const newNote = {
       "title": title,
       "content": content
@@ -42,7 +42,7 @@ function Home() {
     // temp.push(newNote)
     // setNotes(temp);
 
-    if(title ==="" || content ===""){
+    if (title === "" || content === "") {
       swal({
         title: "Error",
         text: "Please fill all the fields",
@@ -65,12 +65,12 @@ function Home() {
 
   return (
     <div>
-      <div className='app-title-container shadow-sm bg-warning '>
-        <h1 className='app-title'>Keep Notes</h1>
+      <div className='app-title-container shadow-sm bg-danger text-light'>
+        <h1 className='app-title'>📝 Sticky Notes </h1>
       </div>
       <div className='row'>
-        <div className='col-md-6'>
-          <div className='notes-container bg-success bg-gradient p-2 text-light bg-opacity-75'>
+        <div className='col-md-6 '>
+          <div className='notes-container bg-secondary  p-2 text-dark bg-opacity-75'>
             {
               notes.map((note, index) => {
                 return (<Note title={note.title} content={note.content} noteIndex={index} />)
@@ -80,20 +80,24 @@ function Home() {
         </div>
 
         <div className='col-md-6'>
-          <div className='note-editor-container bg-success bg-gradient p-2 text-light bg-opacity-75'>
+          <div className='note-editor-container bg-secondary  p-2 text-light bg-opacity-75'>
             <h3 className='text-center'>Add Note</h3>
             <form>
               <div className=''>
-                <input type="text" value={title}  onChange={(e)=>{setTitle(e.target.value)}} className="form-control mt-4" placeholder="Note Title" />
+                <input type="text" value={title} onChange={(e) => { setTitle(e.target.value) }} className="form-control mt-4" placeholder="Note Title" />
               </div>
               <div>
-                <input type="text" value={content} onChange={(e)=>{setContent(e.target.value)}} className="form-control mt-4" placeholder="Note Description" />
+                <input type="text" value={content} onChange={(e) => { setContent(e.target.value) }} className="form-control mt-4" placeholder="Note Description" />
               </div>
               <div className='add-note-button-container'>
                 <button type="button" className="btn btn-danger mt-4" onClick={addNote}>Add Note</button>
               </div>
             </form>
           </div>
+        </div>
+
+        <div className='footer-container text-light mt-5'>
+          <h5>Created By Sakshi | <a href='https://github.com/sakshi-rah/sticky-notes'>Source Code</a> </h5>
         </div>
       </div>
     </div>
